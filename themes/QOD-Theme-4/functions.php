2b45b39ee7bffe63b39f0d26ba2b74f75123bcce
Chris Coyier <chriscoyier@gmail.com>
2019-08-27 07:26:04 -0700

    trying to break cache

--- themes/QOD-Theme-4/functions.php
+++ themes/QOD-Theme-4/functions.php
@@ -9,18 +9,17 @@
     }
 
     wp_enqueue_style('qod-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_script('qod-scripts', get_template_directory_uri() . '/js/min/qod-min.js', array('jquery'), null, true);
+    wp_enqueue_script('qod-scripts', get_template_directory_uri() . '/js/min/qod-min.js', array('jquery'), 1.1, true);
   }
 
   add_action('wp_enqueue_scripts', 'qod_enqueue_scripts');
 
 
-  add_filter( 'rest_post_collection_params', 'my_prefix_add_rest_orderby_params', 10, 1 );
-
-  function my_prefix_add_rest_orderby_params( $params ) {
-      $params['orderby']['enum'][] = 'rand';
-
-      return $params;
+  // This breaks using OTHER params including random, like posts_per_page=1 like we want. Le sigh.
+  add_filter('rest_post_collection_params', 'my_prefix_add_rest_orderby_params', 10, 1);
+  function my_prefix_add_rest_orderby_params($params) {
+    $params['orderby']['enum'][] = 'rand';
+    return $params;
   }
 
 ?>
